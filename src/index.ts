bf6e0a0239829ef15c04335928eaa092c0511a86
chamod-ananda <chamodananda9@gmail.com>
2025-11-20 13:50:31 +0530

    update CORS origin to allow requests from smart-blog-front-end.vercel.app

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["smart-blog-front-end.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
